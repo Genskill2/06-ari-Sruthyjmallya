26add8c84bac4c6c6fd81db108335e686a5fabbd
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-07-01 13:18:48 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -33,7 +33,7 @@ string ari(string paragraph){
     case 7: level = "Seventh Grade";break;
     case 8: level = "Eighth Grade";break;
     case 9: level = "Ninth Grade";break;
-    case 10: level = "Tenth Grade ";break;
+    case 10: level = "Tenth Grade";break;
     case 11: level = "Eleventh Grade";break;
     case 12: level = "Twelfth grade";break;
     case 13: level = "College student";break;
