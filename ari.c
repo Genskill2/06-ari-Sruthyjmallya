f18c07c74ffbe5ad647ba5f26d9a41164ef429d9
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-07-01 12:58:53 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -7,11 +7,12 @@ string ari(string paragraph){
   int n = strlen(paragraph);
   int i = 0,score;
   int words=0,characters=0,sentences=0;
+  string level;
   while(paragraph[i]!='\0'){
     if(isalnum(paragraph[i])){
       characters++;
     }
-    if(paragraph[i]==" "){
+    if(isspace(paragraph[i])){
       words++;
     }
     if(paragraph[i]=='!' || paragraph[i]=='?' || paragraph[i]=='.'){
@@ -23,20 +24,20 @@ string ari(string paragraph){
   
   
   switch(score){
-    case 1: string level = "Kindergarten";break;
-    case 2: string level = "First/Second Grade";break;
-    case 3: string level = "Third Grade";break;
-    case 4: string level = "Fourth Grade ";break;
-    case 5: string level = "Fifth Grade ";break;
-    case 6: string level = "Sixth Grade";break;
-    case 7: string level = "Seventh Grade";break;
-    case 8: string level = "Eighth Grade";break;
-    case 9: string level = "Ninth Grade";break;
-    case 10: string level = "Tenth Grade ";break;
-    case 11: string level = "Eleventh Grade";break;
-    case 12: string level = "Twelfth grade";break;
-    case 13: string level = "College student";break;
-    case 14: string level = "Professor";break;
+    case 1: level = "Kindergarten";break;
+    case 2: level = "First/Second Grade";break;
+    case 3: level = "Third Grade";break;
+    case 4: level = "Fourth Grade ";break;
+    case 5: level = "Fifth Grade ";break;
+    case 6: level = "Sixth Grade";break;
+    case 7: level = "Seventh Grade";break;
+    case 8: level = "Eighth Grade";break;
+    case 9: level = "Ninth Grade";break;
+    case 10: level = "Tenth Grade ";break;
+    case 11: level = "Eleventh Grade";break;
+    case 12: level = "Twelfth grade";break;
+    case 13: level = "College student";break;
+    case 14: level = "Professor";break;
   }
   
   return(level);
